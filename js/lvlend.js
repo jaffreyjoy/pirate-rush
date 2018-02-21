327baca4d8042353ad669e3be8ee31039404e614
Jaffrey Joy <jaffreyjoy@gmail.com>
2018-02-21 15:42:42 +0530

    score text ADDED

--- js/lvlend.js
+++ js/lvlend.js
@@ -1,6 +1,11 @@
 
-
-
+eKilled = 0;
+ekScore = 0;
+accuracy = 0;
+acScore = 0;
+tTaken = 0;
+tTakenScore = 0;
+levelScore = 0;
 
 function preloadLvl() {
     game.load.image('lvl-end', 'assets/ui/level-complete-blue.png');
@@ -14,11 +19,17 @@ function createLvl() {
     hbutton = game.add.button(game.world.centerX - 150, 450, 'hbutton', goToStartPage, this, 1, 0);
     nextLbutton = game.add.button(hbutton.x + 150, hbutton.y, 'nextLbutton', startNextLevel, this, 1, 0);
 
-    EnemiesKilledText = game.add.bitmapText(game.world.centerX - 200, game.world.centerY - 100, 'gem', "Kill Score : ", 30);
+    EnemiesKilledText = game.add.bitmapText(game.world.centerX - 220, game.world.centerY - 130, 'gem', "Enemies killed : "+eKilled+"  ( + "+ekScore+" )", 30);
     EnemiesKilledText.tint = 0x223344;
 
-    AccuracyText = game.add.bitmapText(EnemiesKilledText.x, EnemiesKilledText.y + 50, 'gem', "Accuracy : ", 30);
+    AccuracyText = game.add.bitmapText(EnemiesKilledText.x, EnemiesKilledText.y + 50, 'gem', "Accuracy : " + accuracy + "  ( + " + acScore + " )", 30);
     AccuracyText.tint = 0x223344;
+
+    TimeTakenText = game.add.bitmapText(EnemiesKilledText.x, EnemiesKilledText.y + 100, 'gem', "Time Taken : " + tTaken + "  ( + " + tTakenScore + " )", 30);
+    TimeTakenText.tint = 0x223344;
+
+    LevelScoreText = game.add.bitmapText(EnemiesKilledText.x - 35, EnemiesKilledText.y + 160, 'gem', "Level Score : " + levelScore , 50);
+    LevelScoreText.tint = 0x223344;
 
 }
 
